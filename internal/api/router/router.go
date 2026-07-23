26ce7a95306d8f0e706123e29b793b1e553b75b6
Burhaan <burhaanashraf@outlook.com>
2026-07-24 00:52:33 +0530

    feat(router): integrate all sub-routers, middleware pipeline, and main router configuration

---
+++ internal/api/router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"net/http"
+)
+
+func MainRouter() *http.ServeMux {
+
+	tRouter := TeachersRouter()
+	sRouter := StudentsRouter()
+	eRouter := ExecsRouter()
+
+	sRouter.Handle("/", eRouter)
+	tRouter.Handle("/", sRouter)
+	return tRouter
+
+}
